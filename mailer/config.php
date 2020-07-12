218c98487baedeb4ef93dad45ee7b129e6335180
Charismaforever <charismaforever@users.noreply.github.com>
2020-07-12 14:53:52 -0700

    Json and mailer

---
+++ mailer/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$my_email = 'customerservice@benevolentwoman.com';
+
+$SMTP = array(
+    'enabled' => true,
+    'host' => 'smtp.privateemail.com',
+    'username' => 'customerservice@benevolentwoman.com',
+    'password' => 'password12345',
+    'port' => 465,
+    'encryption' => 'ssl'
+);
